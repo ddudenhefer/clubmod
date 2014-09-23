10bb54790bba6422d5d9d130007093aed5411ad8
David <David@192.168.2.92>
2014-09-22 22:24:59 -0600

    try to access db

--- src/main/java/services/authenticate/AccessToken.java
+++ src/main/java/services/authenticate/AccessToken.java
@@ -1,9 +1,14 @@
 package services.authenticate;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import model.Member;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,22 +23,24 @@ import org.json.JSONObject;
 // The browser requests per default the HTML MIME type.
 
 //Sets the path to base URL + /hello
-@Path("/hello")
-public class Hello {
+@Path("/access")
+public class AccessToken {
+	
+	@Inject 
+	private EntityManager em; 
+
 
   // This method is called if TEXT_PLAIN is request
   @GET
-  @Produces(MediaType.TEXT_PLAIN)
-  public String sayPlainTextHello() {
-    return "Hello Jersey";
+  @Path("{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public String lookupMemberById(@PathParam("id") long id) { 
+	  Member member = em.find(Member.class, id);
+	  JSONObject jObj = new JSONObject();
+	  jObj.put("token", member.getAccessToken());
+	  return jObj.toString();
   }
 
-  // This method is called if XML is request
-  @GET
-  @Produces(MediaType.TEXT_XML)
-  public String sayXMLHello() {
-    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
-  }
 
   // This method is called if HTML is request
   @GET
@@ -42,14 +49,5 @@ public class Hello {
     return "<html> " + "<title>" + "Hello Jersey" + "</title>"
         + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
   }
-  
-//This method is called if JSON is request
- @GET
- @Produces(MediaType.APPLICATION_JSON)
- public String sayJsonlHello() throws JSONException {
-	 JSONObject jObj = new JSONObject();
-	 jObj.put("hello", "Hello Jersey");
-	 return jObj.toString();
- }
 
 } 
