2a3d9c6559a6c266b1254a1bd4c78e0711eb481a
David <David@192.168.2.92>
2014-09-23 06:29:20 -0600

    use hibernate and jpa

--- src/main/java/services/authenticate/AccessToken.java
+++ src/main/java/services/authenticate/AccessToken.java
@@ -1,18 +1,16 @@
 package services.authenticate;
 
-import java.util.ArrayList;
-
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import model.MemberManager;
+import model.Member;
 
 import com.google.gson.Gson;
-
-import dto.MemberDTO;
 
 // Plain old Java Object it does not extend as class or implements 
 // an interface
@@ -27,26 +25,20 @@ import dto.MemberDTO;
 @Path("/access")
 public class AccessToken {
 	
+	@Inject
+	private EntityManager em;
+	
   // This method is called if TEXT_PLAIN is request
   @GET
   @Path("{id}")
   @Produces(MediaType.APPLICATION_JSON)
   public String lookupMemberById(@PathParam("id") long id) { 
-	  
-	  	ArrayList<MemberDTO> feedData = null;
-		MemberManager projectManager= new MemberManager();
-		try {
-			feedData = projectManager.GetFeeds();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Gson gson = new Gson();
-		System.out.println(gson.toJson(feedData));
-		String feeds = gson.toJson(feedData);	  
-		return feeds;
+	  Member member = em.find(Member.class, id);
+	  Gson gson = new Gson();
+	  System.out.println(gson.toJson(member));
+	  String ret = gson.toJson(member);	  
+		return ret;
   }
-
 
   // This method is called if HTML is request
   @GET
