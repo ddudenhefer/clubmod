237a01d4753d196edc9754bac27e52b939931d47
David <David@192.168.2.92>
2014-09-23 21:43:58 -0600

    fixed database name

--- src/main/java/dao/Database.java
+++ src/main/java/dao/Database.java
@@ -10,7 +10,7 @@ public class Database {
     public static final String MYSQL_PASSWORD = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
     public static final String MYSQL_DATABASE_HOST = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
     public static final String MYSQL_DATABASE_PORT = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-    public static final String MYSQL_DATABASE_NAME = "my_schema";
+    public static final String MYSQL_DATABASE_NAME = "services";
     //public static final String MYSQL_DATABASE_DRIVER = "com.mysql.jdbc.Driver";
     private static Connection con;
 
