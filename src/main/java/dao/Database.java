e0ab039ed299161990dbf54846a521dbb02d230f
David <David@192.168.2.92>
2014-10-02 18:49:49 -0600

    test

--- src/main/java/dao/Database.java
+++ src/main/java/dao/Database.java
@@ -11,7 +11,7 @@ public class Database {
     public static final String MYSQL_DATABASE_HOST = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
     public static final String MYSQL_DATABASE_PORT = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
     public static final String MYSQL_DATABASE_NAME = "services";
-    //public static final String MYSQL_DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String MYSQL_DATABASE_DRIVER = "com.mysql.jdbc.Driver";
     private static Connection con;
 
 	    public static String initConnection(){
@@ -19,7 +19,7 @@ public class Database {
 	        if(con==null){
 	            try {
 	                url = "jdbc:mysql://" + MYSQL_DATABASE_HOST + ":" + MYSQL_DATABASE_PORT + "/" + MYSQL_DATABASE_NAME;
-                    Class.forName("com.mysql.jdbc.Driver");
+                    Class.forName(MYSQL_DATABASE_DRIVER);
 	                con = DriverManager.getConnection(url, MYSQL_USERNAME, MYSQL_PASSWORD);
 	            } catch (SQLException e) {
 	                e.printStackTrace();
