72eda236c2d2fede1cd077dba840d0a7e11104d2
David <David@192.168.2.92>
2014-11-05 20:27:15 -0700

    added points service

--- src/main/java/dao/PointsDAO.java
+++ src/main/java/dao/PointsDAO.java
@@ -1,17 +1,88 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import entities.activity.Activity;
 import model.Challenge;
+import model.Member;
 import model.Point;
 
 public class PointsDAO {
+	
+	
+	public Point getPointById(int id)throws Exception {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		Point point = null;
+
+		try {
+			connection = Database.getConnection();
+			if (connection != null) {
+				preparedStatement = connection.prepareStatement("SELECT id, type, subtype, increment, maxlimit, points FROM points where id=?");
+				preparedStatement.setLong(1,id);
+				ResultSet rs = preparedStatement.executeQuery();
+				if (rs.next()) {
+					point = new Point();
+					point.setId(rs.getInt("id"));
+					point.setType(rs.getString("type"));
+					point.setSubType(rs.getString("subtype"));
+					point.setIncrement(rs.getInt("increment"));
+					point.setLimit(rs.getInt("maxlimit"));
+					point.setPoints(rs.getInt("points"));
+				}
+			}
+		
+		} catch (Exception e) {
+			throw e;
+		}
+		finally {
+			if (preparedStatement != null)
+				preparedStatement.close();
+			if (connection != null)
+				connection.close();
+		}
+		return point;
+	}
+	
+	public List<Point> getAllPoints()throws Exception {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		List<Point> points = new ArrayList<Point>();
+		Point point = null;
+
+		try {
+			connection = Database.getConnection();
+			if (connection != null) {
+				String sql = "SELECT id, type, subtype, increment, maxlimit, points FROM points";
+				preparedStatement = connection.prepareStatement(sql);
+				ResultSet rs = preparedStatement.executeQuery();
+				if (rs.next()) {
+					point = new Point();
+					point.setId(rs.getInt("id"));
+					point.setType(rs.getString("type"));
+					point.setSubType(rs.getString("subtype"));
+					point.setIncrement(rs.getInt("increment"));
+					point.setLimit(rs.getInt("maxlimit"));
+					point.setPoints(rs.getInt("points"));
+					points.add(point);
+				}
+			}
+		
+		} catch (Exception e) {
+			throw e;
+		}
+		finally {
+			if (preparedStatement != null)
+				preparedStatement.close();
+			if (connection != null)
+				connection.close();
+		}
+		return points;
+	}
+
 	
 	public Point getPoints(String type, String subType)throws Exception {
 		Connection connection = null;
@@ -76,5 +147,78 @@ public class PointsDAO {
 
 		return points;
 	}
+	
+	public boolean savePoint(Point point)throws Exception {
+		
+		if (point == null)
+			return false;
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = Database.getConnection();
+			if (connection != null) {
+				Point pointDB = getPointById(point.getId());
+				if (pointDB != null) {	// update
+					String sql = "update points set increment=?, maxlimit=?, points=? where id=?";
+					preparedStatement = connection.prepareStatement(sql);
+					preparedStatement.setInt(1, point.getIncrement());
+					preparedStatement.setInt(2, point.getLimit());
+					preparedStatement.setInt(3, point.getPoints());
+					preparedStatement.setLong(4, point.getId());
+		
+					int rowsAffected = preparedStatement.executeUpdate();
+					if (rowsAffected > 0)
+						return true;
+				}
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+		finally {
+			if (preparedStatement != null)
+				preparedStatement.close();
+			if (connection != null)
+			connection.close();
+		}
+		return false;
+	}
+
+	public boolean deletePoint(Point point)throws Exception {
+		
+		if (point == null || point.getId() == 0)
+			return false;
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			connection = Database.getConnection();
+			if (connection != null) {
+				Point pointDB = getPointById(point.getId());
+				if (pointDB != null) {
+					String sql = "delete from points where id=?";
+					preparedStatement = connection.prepareStatement(sql);
+					preparedStatement.setLong(1, point.getId());
+		
+					int rowsAffected = preparedStatement.executeUpdate();
+					if (rowsAffected > 0)
+						return true;
+				}
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+		finally {
+			if (preparedStatement != null)
+				preparedStatement.close();
+			if (connection != null)
+				connection.close();
+		}
+		
+		return false;
+	}
+	
 	
 }
---
+++ src/main/java/services/PointSvc.java
@@ -0,0 +1,104 @@
+package services;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Point;
+import com.google.gson.Gson;
+import dao.PointsDAO;
+
+@Path("/member")
+public class PointSvc {
+	
+	@GET
+	@Path("{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getPointById(@PathParam("id") int id) { 
+		Point point = null;
+		
+		try {
+			point = new PointsDAO().getPointById(id);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		Gson gson = new Gson();
+		String ret = "";
+		if (point != null) {
+			System.out.println(gson.toJson(point));
+			ret = gson.toJson(point);
+		}
+		else {
+			ret = gson.toJson("");
+		}
+		return ret;
+	}
+
+	
+	@GET
+	@Path("/all")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAllMembers() {
+		List<Point> points = new ArrayList<Point>();
+		
+		try {
+			points = new PointsDAO().getAllPoints();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		Gson gson = new Gson();
+		String ret = "";
+		ret = gson.toJson(points);
+		return ret;
+	}
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/update")
+	public boolean savePoints(final Point point) {
+		PointsDAO pointsDAO = new PointsDAO();
+		boolean ret = false;
+		
+		try {
+			ret = pointsDAO.savePoint(point);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ret = false;
+		}
+	    return ret; 
+	}
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/delete")
+	public boolean deleteMember(final Point point) {
+		PointsDAO pointsDAO = new PointsDAO();
+		boolean ret = false;
+
+		try {
+			ret = pointsDAO.deletePoint(point);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ret = false;
+		}
+	    return ret; 
+	}	
+} 
