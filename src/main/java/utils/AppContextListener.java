482fed6d34be0027db215445e90f5b9a8d132f41
David <David@Livingroom>
2016-04-25 21:47:36 -0600

    enable weekly challenge

--- src/main/java/utils/AppContextListener.java
+++ src/main/java/utils/AppContextListener.java
@@ -58,9 +58,9 @@ public class AppContextListener implements ServletContextListener {
 		//groupRideTimer.scheduleAtFixedRate(updateGroupRideTask, getRunDate(Calendar.SUNDAY, 20), ONCE_PER_WEEK);
 		//groupRideTimer.schedule(updateGroupRideTask, 0);
 
-		//TimerTask updateChallengeWinnerTask = new UpdateChallengeWinnerTask();
-		//Timer challengeWinnerTimer = new Timer();
-		//challengeWinnerTimer.scheduleAtFixedRate(updateChallengeWinnerTask, getRunDate(Calendar.MONDAY, 14), ONCE_PER_WEEK);
+		TimerTask updateChallengeWinnerTask = new UpdateChallengeWinnerTask();
+		Timer challengeWinnerTimer = new Timer();
+		challengeWinnerTimer.scheduleAtFixedRate(updateChallengeWinnerTask, getRunDate(Calendar.MONDAY, 14), ONCE_PER_WEEK);
 		//challengeWinnerTimer.schedule(updateChallengeWinnerTask, 0);
 
 		TimerTask updateMemberYTDTask = new UpdateMemberYTDTask();
