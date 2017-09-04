3a0756662aa524ed294c5af46a6b79b0293269b6
David <David@Livingroom>
2017-09-04 08:21:13 -0600

    updated username/password

--- src/main/java/dao/Database.java
+++ src/main/java/dao/Database.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public class Database {
 	
-    public static final String MYSQL_USERNAME = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-    public static final String MYSQL_PASSWORD = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-    public static final String MYSQL_DATABASE_HOST = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-    public static final String MYSQL_DATABASE_PORT = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+    public static final String MYSQL_USERNAME = "adminGK9GXfL";	//System.getenv("MYSQL_USER");
+    public static final String MYSQL_PASSWORD = "jLJwZbrJHGQ4";	//System.getenv("MYSQL_PASSWORD");
+    public static final String MYSQL_DATABASE_HOST = System.getenv("MYSQL_SERVICE_HOST");
+    public static final String MYSQL_DATABASE_PORT = System.getenv("MYSQL_SERVICE_PORT");
     public static final String MYSQL_DATABASE_NAME = "services";
     public static final String MYSQL_DATABASE_DRIVER = "com.mysql.jdbc.Driver";
 
@@ -17,6 +17,10 @@ public class Database {
 	        String url = "";
 	        Connection con = null;
             try {
+            	System.out.println("user: " + MYSQL_USERNAME);
+            	System.out.println("pass: " + MYSQL_PASSWORD);
+            	System.out.println("host: " + MYSQL_DATABASE_HOST);
+            	System.out.println("port: " + MYSQL_DATABASE_PORT);
                 url = "jdbc:mysql://" + MYSQL_DATABASE_HOST + ":" + MYSQL_DATABASE_PORT + "/" + MYSQL_DATABASE_NAME;
                 Class.forName(MYSQL_DATABASE_DRIVER);
                 con = DriverManager.getConnection(url, MYSQL_USERNAME, MYSQL_PASSWORD);
